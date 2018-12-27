67e9b9940fb898d9ddae81f774b51454be68f2e6
ae <90932525+ea-ae@users.noreply.github.com>
2018-12-27 12:45:45 +0200

    Added more details to the status message.

--- thecamp.py
+++ thecamp.py
@@ -5,17 +5,16 @@ import prettytable
 from config import TOKEN
 
 
-channels = {}
-settings = {
-    'SERVER_ID': '527261834852696064'
-}
+# Settings
+SERVER_ID = '527261834852696064'
 
 client = discord.Client()
+channels = {}
 
 
 @client.event
 async def on_ready():
-    server = client.get_server(settings['SERVER_ID'])
+    server = client.get_server(SERVER_ID)
     for channel in server.channels:
         channels[channel.name] = client.get_channel(channel.id)
 
@@ -27,24 +26,43 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    asyncio.ensure_future(generate_status_message())
     if message.content.startswith('!'):
         parts = message.content[1:].split()
-        if parts[0] == 'hello':
-            await client.send_message(message.channel, 'world!')
-        elif parts[0] == 'help':
-            await client.send_message(message.channel, 'There is no help to be had.')
+        if parts[0] == 'say':
+            if 'Moderator' in [role.name for role in message.author.roles]:
+                await client.send_message(message.channel, ' '.join(parts[1:]))
 
-    # Temporary tables with fake data
-    status_table = prettytable.PrettyTable(['Resource', 'Amount'])
+
+async def generate_status_message():
+    # Temporary fake data
+    food = 725
+    fuel = 205
+    medicine = 53
+    materials = 2509
+    scrap = 9487
+
+    days = 0
+    temperature = -32
+    defense = 5423
+
+    status_table = prettytable.PrettyTable(['Name', 'Value'])
     status_table.align = 'r'
-    status_table.add_row(['Food', 725])
-    status_table.add_row(['Fuel', 611])
-    status_table.add_row(['Materials', 2509])
-    status_table.add_row(['Medicine', 53])
+    status_table.add_row(['Days survived', days])
+    status_table.add_row(['Temperature', str(temperature) + '°C'])
+    status_table.add_row(['Defense points', '{:,}'.format(defense)])
+
+    warehouse_table = prettytable.PrettyTable(['Resource', 'Amount'])
+    warehouse_table.align = 'r'
+    warehouse_table.add_row(['Food', '{:,}'.format(food)])
+    warehouse_table.add_row(['Fuel', '{:,}'.format(fuel)])
+    warehouse_table.add_row(['Medicine', '{:,}'.format(medicine)])
+    warehouse_table.add_row(['Materials', '{:,}'.format(materials)])
+    warehouse_table.add_row(['Scrap', '{:,}'.format(scrap)])
 
     async for message in client.logs_from(channels['camp-status']):
         if message.author == client.user:
-            status = '**Camp Resources**```' + str(status_table) + '```'
+            status = '**Camp Status**```' + str(status_table) + '```**Warehouse**```' + str(warehouse_table) + '```'
             await client.edit_message(message, status)
             break
 
