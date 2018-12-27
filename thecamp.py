1b92f3df7494ac7b7ca25950c56ad6a1ec4f68bc
ae <90932525+ea-ae@users.noreply.github.com>
2018-12-27 15:56:04 +0200

    Check for user roles.

--- thecamp.py
+++ thecamp.py
@@ -4,11 +4,8 @@ from discord.ext import commands
 
 import prettytable
 
-from config import TOKEN
+from config import TOKEN, SERVER_ID
 
-
-# Settings
-SERVER_ID = '527261834852696064'
 
 client = commands.Bot(command_prefix='!')
 client.remove_command('help')
@@ -27,6 +24,7 @@ async def on_ready():
     print('Logged in as:')
     print(client.user.name)
     print(client.user.id)
+
     await client.change_presence(game=discord.Game(name='Survived for 0 days'))
 
 
@@ -41,18 +39,30 @@ To join the camp, simply type `!join`.""").format(member.mention)
 @client.event
 async def on_command_error(error, ctx):
     if isinstance(error, commands.CommandNotFound):
-        pass
+        await client.say('Unknown command. Type `!help` for a list of commands.')
 
 
-@client.command()
-@commands.has_any_role('Moderator')  # This only works if the command is invoked in the server!
-async def say(*, msg):
-    await client.say(msg)
+@client.command(pass_context=True)
+async def say(ctx, *, msg):
+    if await user_has_role(ctx.message.author, 'Moderator'):
+        await client.say(msg)
 
 
 @client.command()
 async def help():
     await client.say('I don\'t need any help from you right now, but thanks for asking.')
+
+
+@client.command(pass_context=True)
+async def join(ctx):
+    if await user_has_role(ctx.message.author, 'Alive'):
+        await client.say('You have already joined the camp.')
+    elif await user_has_role(ctx.message.author, 'Dead'):
+        await client.say('You have already joined this game. Since you are dead, you have to wait till someone revives '
+                         'you or the next game starts.')
+    else:
+        await client.say('Welcome to **The Camp**! If it is your first time here, be sure to read the tutorial channel '
+                         'first. To see a list of available commands, type `!help`.')
 
 
 async def generate_status_message():
@@ -86,6 +96,21 @@ async def generate_status_message():
             status = '**Camp Status**```{0}```**Warehouse**```{1}```'.format(status_table, warehouse_table)
             await client.edit_message(message, status)
             break
+
+
+async def user_has_role(user, *args):
+    """
+    Takes in a User object and any amount of role names.
+    Checks if the user has any of the roles listed.
+    """
+    return any([role in await get_user_roles(user) for role in args])
+
+
+async def get_user_roles(user):
+    """
+    Takes in an User object and returns his roles in the server.
+    """
+    return [role.name for role in server.get_member(user.id).roles]
 
 # Run the bot
 client.run(TOKEN)
